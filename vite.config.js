0557e9836d518e25364015501ee889aa16e07d37
meongj <meongjiee@gmail.com>
2026-04-07 22:51:45 +0900

    chore: initialize project with Tailwind, shadcn, TanStack Router

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,11 @@ import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react(), tailwindcss()],
+  plugins: [
+    TanStackRouterVite({quoteStyle: "single", target: "react", autoCodeSplitting: true}),
+    react(),
+    tailwindcss(),
+  ],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
