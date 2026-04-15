9a5b6444aec7bc58f3a059bfcfd052866b02cd05
meongj <meongjiee@gmail.com>
2026-04-15 14:51:48 +0900

    feat : list fields 정리 + detail/borders 팩토리 추가

--- src/queries/countries.ts
+++ src/queries/countries.ts
@@ -2,9 +2,13 @@ import {api} from "@/lib/axios";
 import {Country} from "@/types/country";
 import {queryOptions} from "@tanstack/react-query";
 
+const DAY = 1000 * 60 * 60 * 24;
+
 // 국가 데이터
 export const countryKeys = {
   all: ["countries"] as const,
+  detail: (code: string) => [...countryKeys.all, "detail", code] as const,
+  borders: (codes: string[]) => [...countryKeys.all, "borders", [...codes].sort()] as const,
 };
 
 export const countryQueries = {
@@ -12,12 +16,28 @@ export const countryQueries = {
     queryOptions({
       queryKey: countryKeys.all,
       queryFn: async () => {
-        return await api
-          .get<
-            Country[]
-          >("/all?fields=name,population,area,borders,flags,independent,unMember,continents,region,subregion")
+        await api
+          .get<Country[]>("/all?fields=cc3,name,population,area,borders,flags,independent,unMember,region,subregion")
           .then((res) => res.data);
       },
-      staleTime: 1000 * 60 * 60 * 24, // 24시간
+      staleTime: DAY,
+    }),
+  detail: (code: string) =>
+    queryOptions({
+      queryKey: countryKeys.detail(code),
+      queryFn: async () => {
+        await api.get<Country[]>(
+          `/alpha?codes=${code}&fields=cca3,name,capital,population,area,borders,flags,independent,unMember,languages,currencies,region,subregion`,
+        );
+      },
+      staleTime: DAY,
+      enabled: !!code,
+    }),
+  borders: (codes: string[]) =>
+    queryOptions({
+      queryKey: countryKeys.borders(codes),
+      queryFn: async () => await api.get<Country[]>(`/alpha?codes=${codes.join(",")}&fields=cca3,name,flags`).data,
+      staleTime: DAY,
+      enabled: codes.length > 0,
     }),
 };
